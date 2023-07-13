4014509fae7124c5c0be21f9dfc59c09b2790575
Moyo-tech <m.weke@alustudent.com>
2023-07-13 20:10:50 +0200

    added training session list

--- src/features/tgdetail.js/Tgtabs.js
+++ src/features/tgdetail.js/Tgtabs.js
@@ -5,6 +5,8 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Tgtabdetail from "./Tgtabdetail";
+import Tgtabtable from "./tgtabtable";
+
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -63,7 +65,7 @@ export default function BasicTabs() {
         <Tgtabdetail />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        Item Two
+        <Tgtabtable />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
         Item Three
