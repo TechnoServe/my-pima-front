9321e22dc80e278d7eae429027e42479431a20c0
Moyo-tech <m.weke@alustudent.com>
2023-07-20 10:50:59 +0200

    added statistics componenent

--- src/features/statsts/Statstscard.js
+++ src/features/statsts/Statstscard.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./tscard.css";
 import { MdMan, MdWoman } from "react-icons/md";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 
 const Statstscard = () => {
+  const percentage = 66;
+
   return (
     <div>
       <div className="cardts__container">
@@ -14,10 +20,8 @@ const Statstscard = () => {
         <div style={{ display: "flex" }}>
           <div style={{ marginRight: "50px" }}>
             <div className="mafa__attendance" style={{ paddingBottom: "10px" }}>
-              <div
-                className="cardts__icon"
-              >
-                <MdMan  size={22}/>
+              <div className="cardts__icon">
+                <MdMan size={22} />
               </div>
               <div>
                 <p className="attend__text">Male Attendance</p>
@@ -25,10 +29,8 @@ const Statstscard = () => {
               </div>
             </div>
             <div className="mafa__attendance">
-              <div
-                className="cardts__icon"
-              >
-                <MdWoman size={22}/>
+              <div className="cardts__icon">
+                <MdWoman size={22} />
               </div>
               <div>
                 <p className="attend__text">Female Attendance</p>
@@ -36,7 +38,25 @@ const Statstscard = () => {
               </div>
             </div>
           </div>
-          <div className="total__attendance">75% Attendance</div>
+          <div style={{ width: "100px" ,height:"50%" }}>
+            <CircularProgressbarWithChildren
+              value={percentage}
+              styles={buildStyles({
+                rotation: 0.5,
+                strokeLinecap: "round",
+                textSize: "20px",
+                pathTransitionDuration: 0.5,
+                pathColor: "rgba(244, 103, 0, 1)",
+                textColor: "rgba(244, 103, 0, 1)",
+                trailColor: "#ECF3FE",
+              })}
+            >
+            <div style={{position: "absolute", top: "15px"}}>
+              <p style={{fontSize: "20px", textAlign:"center", color: "#F46700", fontWeight: "600"}}>{percentage} %</p>
+              <p style={{fontSize: "10px", fontWeight: "500", color: "" }}>Attendance</p>
+           </div>
+            </CircularProgressbarWithChildren>
+          </div>
         </div>
       </div>
     </div>
