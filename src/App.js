6ca6ba3c0d6b9c13ae87e198229600a6203c8ec6
julesntare <julesntare@gmail.com>
2023-08-16 22:53:20 +0200

    added favicon with title change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Login from "./pages/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import theme from "./components/Theme";
 import { ThemeProvider } from "@mui/material";
+import { useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
+      <TitleUpdater />
       <ThemeProvider theme={theme}>
         <Routes>
           <Route exact path="/*" element={<Navbar />} />
@@ -16,6 +18,18 @@ function App() {
       </ThemeProvider>
     </div>
   );
+}
+
+function TitleUpdater() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname; // Get the current path
+    const pageName = path === "/" ? "Home" : path.substring(1).toUpperCase(); // Extract page name from path
+    document.title = `My PIMA - ${pageName}`; // Update the title
+  }, [location]);
+
+  return null;
 }
 
 export default App;
